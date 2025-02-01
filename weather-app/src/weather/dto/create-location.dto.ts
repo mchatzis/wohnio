c44526b52fedd96ecadf72a3fbba7ec11dda0f21
Michail Chatzis <mchatzis3@gmail.com>
2025-02-01 20:42:54 +0100

    Added swagger openapi

--- weather-app/src/weather/dto/create-location.dto.ts
+++ weather-app/src/weather/dto/create-location.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateLocationDto {
+    @ApiProperty()
     @IsNotEmpty()
     @IsNumber()
     latitude: number;
 
+    @ApiProperty()
     @IsNotEmpty()
     @IsNumber()
     longitude: number;
 
+    @ApiProperty()
     @IsOptional()
     @IsString()
     name?: string;
